b92626ecbaadb984706d1bad515aa996f925317b
lotrapan <lotrapan@gmail.com>
2024-05-31 18:47:18 +0200

    main modificato

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,8 +6,16 @@
 /*   By: lotrapan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 14:02:57 by lotrapan          #+#    #+#             */
-/*   Updated: 2024/05/29 17:44:58 by lotrapan         ###   ########.fr       */
+/*   Updated: 2024/05/30 17:05:31 by lotrapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	builtin_echo(char *message, bool flag)
+{
+	if (!flag)
+		ft_printf(1, "%s", message);
+	else
+		ft_printf(1, "%s\n", message);
+}
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lotrapan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 14:01:13 by lotrapan          #+#    #+#             */
-/*   Updated: 2024/05/29 14:04:10 by lotrapan         ###   ########.fr       */
+/*   Updated: 2024/05/31 18:24:55 by lotrapan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int main()
 {
 	char	*shell;
 	(void)shell;
+	char	**av;
 	// t_builtin	ptr;
 	// ptr = (t_builtin){0};
 	// builtin_pwd(&ptr);
@@ -23,6 +24,14 @@ int main()
 	while (42)
 	{
 		shell = readline("Minishell > ");
+		av = ft_split(shell, ' ');
+		if (!av)
+			builtin_exit(NULL);
+		if (ft_strncmp(av[0], "exit", 5) == 0)
+			builtin_exit(av);
+		if (ft_strncmp(av[0], "echo", 5) == 0)
+			builtin_echo(av[0] + 5, true);
+		free(shell);
 		// parse_line(shell);
 
 	}
